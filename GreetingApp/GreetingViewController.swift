8a10e88ee58e9d34b20ab5f62cf66fa01b9ad516
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-11-07 19:47:28 +0300

    Implemented app functionality according to MVP.

--- GreetingApp/GreetingViewController.swift
+++ GreetingApp/GreetingViewController.swift
@@ -7,18 +7,31 @@
 
 import UIKit
 
+protocol GreetingViewProtocol: AnyObject {
+    func setGreeting(_ greeting: String)
+}
+
 class GreetingViewController: UIViewController {
 
     @IBOutlet var greetingLabel: UILabel!
     
+    private var presenter: GreetingPresenterProtocol!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let person = Person(name: "Tim", surname: "Cook")
+        presenter = GreetingPresenter(view: self, person: person)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
+        presenter.showGreeting()
     }
+}
 
-
+extension GreetingViewController: GreetingViewProtocol {
+    func setGreeting(_ greeting: String) {
+        greetingLabel.text = greeting
+    }
 }
 
