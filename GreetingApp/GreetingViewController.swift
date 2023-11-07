3f199c71019783d2e5899557251a8987ece32f0d
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-11-07 11:09:08 +0300

    Initial project setup.

---
+++ GreetingApp/GreetingViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GreetingApp
+//
+//  Created by Lera Savchenko on 7.11.23.
+//
+
+import UIKit
+
+class GreetingViewController: UIViewController {
+
+    @IBOutlet var greetingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+    }
+
+
+}
+
