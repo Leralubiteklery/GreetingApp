6b88d33e68522e4e5eec4ee76db1cb901bfd32b7
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-11-07 11:37:18 +0300

    Implemented app functionality according to MVC.

--- GreetingApp/GreetingViewController.swift
+++ GreetingApp/GreetingViewController.swift
@@ -11,12 +11,16 @@ class GreetingViewController: UIViewController {
 
     @IBOutlet var greetingLabel: UILabel!
     
+    private var person: Person!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        person = Person(name: "Tim", surname: "Cook")
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
+        greetingLabel.text = "Hello \(person.name) \(person.surname)!"
     }
 
 
